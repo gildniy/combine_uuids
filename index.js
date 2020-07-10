1a65c3d1e65931309554f3e2eea64160352283d1
Gildas Niyigena <gildniy@users.noreply.github.com>
2020-07-10 23:26:31 +0200

    Create README.md

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// module.exports.area = radius => Math.PI * (radius ** 2)
-// module.exports.perimeter = radius => Math.PI * radius * 2
-module.exports.uuids = (first, two, version = 'v4') => {
+module.exports.uuids = (first, second, version = 'v4') => {
     if (version === 'v4') {
-        return first.slice(0, 18) + two.slice(two.length - 18)
+        return first.slice(0, 18) + second.slice(second.length - 18)
     }
 }
